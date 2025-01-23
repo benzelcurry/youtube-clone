db0338475f72ec761f629be6ca6c9bee2e7401a4
Ben <benawhiting@gmail.com>
2025-01-23 15:52:59 -0800

    Move .gitignore

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,11 +1,30 @@
 import express from "express";
+import ffmpeg from "fluent-ffmpeg";
 
 const app = express();
-const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.post("/process-video", (req, res) => {
+  // Get path of the input video file from the request body
+  const inputFilePath = req.body.inputVideoPath;
+  const outputFilePath = req.body.outputFilePath;
+
+  if (!inputFilePath || !outputFilePath) {
+    res.status(400).send("Bad Request: Missing file path.");
+  }
+
+  ffmpeg(inputFilePath)
+    .outputOptions("-vf", "scale=-1:360") // 360p
+    .on("end", () => {
+      res.status(200).send("Video processing finished successfully.");
+    })
+    .on("error", (err) => {
+      console.log(`An error occurred: ${err.message}`);
+      res.status(500).send(`Internal Server Error: ${err.message}`);
+    })
+    .save(outputFilePath);
 });
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Video processing service listening at http://localhost:${port}`);
