12349a9b563880080bf849f3d47301c5658a54d6
Ben <benawhiting@gmail.com>
2025-01-27 15:00:41 -0800

    Move code for handling video processing

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -12,20 +12,6 @@ app.post("/process-video", (req, res) => {
   if (!inputFilePath || !outputFilePath) {
     res.status(400).send("Bad Request: Missing file path.");
   }
-
-  ffmpeg(inputFilePath)
-    .outputOptions(
-      "-vf",
-      "scale='if(gte(iw/ih,1),floor(360*iw/ih/2)*2,360)':'if(gte(iw/ih,1),360,floor(360*ih/iw/2)*2)'"
-    ) // Adjust width to be divisible by 2 and then convert to 360p
-    .on("end", () => {
-      res.status(200).send("Video processing finished successfully.");
-    })
-    .on("error", (err) => {
-      console.log(`An error occurred: ${err.message}`);
-      res.status(500).send(`Internal Server Error: ${err.message}`);
-    })
-    .save(outputFilePath);
 });
 
 const port = process.env.PORT || 3000;
--- video-processing-service/src/storage.ts
+++ video-processing-service/src/storage.ts
@@ -2,6 +2,14 @@ import { Storage } from "@google-cloud/storage";
 import fs from "fs";
 import ffmpeg from "fluent-ffmpeg";
 
+const storage = new Storage();
+
+const rawVideoBucketName = "benzel-yt-raw-videos";
+const processedVideoBucketName = "benzel-yt-processed-videos";
+
+const localRawVideoPath = "./raw-videos";
+const localProcessedVideoPath = "./processed-videos";
+
 /**
  * Creates the local directories for raw and processed videos.
  */
@@ -14,7 +22,21 @@ export function setupDirectories() {}
  * @returns A promise that resolves when the video has been converted.
  */
 
-export function convertVideo(
-  rawVideoName: string,
-  processedVideoName: string
-) {}
+export function convertVideo(rawVideoName: string, processedVideoName: string) {
+  return new Promise<void>((resolve, reject) => {
+    ffmpeg(`${localRawVideoPath}/${rawVideoName}`)
+      .outputOptions(
+        "-vf",
+        "scale='if(gte(iw/ih,1),floor(360*iw/ih/2)*2,360)':'if(gte(iw/ih,1),360,floor(360*ih/iw/2)*2)'"
+      ) // Adjust width to be divisible by 2 and then convert to 360p
+      .on("end", () => {
+        console.log("Video processing finished successfully.");
+        resolve();
+      })
+      .on("error", (err) => {
+        console.log(`An error occurred: ${err.message}`);
+        reject(err);
+      })
+      .save(`${localProcessedVideoPath}/${processedVideoName}`);
+  });
+}
