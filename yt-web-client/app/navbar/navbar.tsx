99397abbc79b0a2aff06ccdead3b476d96bcdfe7
Ben <benawhiting@gmail.com>
2025-02-07 14:47:05 -0800

    Set up navbar to handle sign in/sign out

---
+++ yt-web-client/app/navbar/navbar.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+
+import SignIn from "./signin";
+import { onAuthStateChangedHelper } from "../firebase/firebase";
+
+import styles from "./navbar.module.css";
+import { useEffect, useState } from "react";
+import { User } from "firebase/auth";
+
+export default function Navbar() {
+  // Initialize user state
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedHelper((user) => {
+      setUser(user);
+    });
+
+    // Clean up subscription on unmount
+    return () => unsubscribe();
+  });
+
+  return (
+    <nav className={styles.logo}>
+      <Link href="/" className={styles.logoContainer}>
+        <h1>Home</h1>
+      </Link>
+      <SignIn user={user} />
+    </nav>
+  );
+}
